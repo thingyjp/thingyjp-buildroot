81fd1bb1ab6382184c16b5453c697e5420c915a3
Daniel Palmer <daniel@0x0f.com>
2018-07-18 17:43:04 +0900

    Add timestamp to builds. Tweak busybox config.

--- post_build.sh
+++ post_build.sh
@@ -2,17 +2,32 @@
 set -u
 set -x
 TARGETDIR="output/target"
-rm -fv $TARGETDIR/usr/bin/cvtsudoers \
+rm -frv $TARGETDIR/usr/bin/cvtsudoers \
 	$TARGETDIR/usr/bin/gapplication \
 	$TARGETDIR/usr/bin/gdbus \
 	$TARGETDIR/usr/bin/gio \
 	$TARGETDIR/usr/bin/gio-querymodules \
+	$TARGETDIR/usr/bin/gsettings \
+	$TARGETDIR/usr/bin/gresource \
 	$TARGETDIR/usr/bin/json-glib-format \
 	$TARGETDIR/usr/bin/json-glib-validate \
 	$TARGETDIR/usr/bin/nettle-hash \
 	$TARGETDIR/usr/bin/nettle-lfib-stream \
 	$TARGETDIR/usr/bin/nettle-pbkdf2 \
 	$TARGETDIR/usr/bin/pcretest \
+	$TARGETDIR/usr/bin/asn1Coding \
+	$TARGETDIR/usr/bin/asn1Decoding \
+	$TARGETDIR/usr/bin/asn1Parser \
+	$TARGETDIR/usr/bin/rngtest \
+	$TARGETDIR/usr/bin/sudoreplay \
+	$TARGETDIR/usr/bin/pcregrep \
+	$TARGETDIR/usr/bin/pkcs1-conv \
 	$TARGETDIR/usr/sbin/visudo \
-	$TARGETDIR/usr/sbin/wpa_cli
+	$TARGETDIR/usr/sbin/wpa_cli \
+	$TARGETDIR/etc/services \
+	$TARGETDIR/etc/wpa_supplicant.conf \
+	$TARGETDIR/usr/share/glib-2.0/valgrind/glib.supp \
+	$TARGETDIR/usr/libexec/installed-tests \
+	$TARGETDIR/usr/share/installed-tests
 
+date -u +"%Y-%m-%d %H:%M:%S" > $TARGETDIR/etc/buildstamp
