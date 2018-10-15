70c5868de4db4b6182113e0f740fd53d1a2a86ab
Daniel Palmer <daniel@0x0f.com>
2018-10-15 22:32:57 +0900

    Various packaging fixes and cleanups

--- package/thingyjp-ota/thingyjp-ota.mk
+++ package/thingyjp-ota/thingyjp-ota.mk
@@ -16,23 +16,38 @@ HOST_THINGYJP_OTA_GIT_SUBMODULES = YES
 HOST_THINGYJP_OTA_CONF_OPTS += -Dhost=true
 
 define THINGYJP_OTA_INSTALLKEYS
+	if [ ! -d $(BR2_PACKAGE_THINGYJP_OTA_KEYSDIR) ]; then \
+		echo "keys dir doesn't exist, creating it, you probably don't want this!"; \
+		$(HOST_DIR)/bin/ota_keygen -k $(BR2_PACKAGE_THINGYJP_OTA_KEYSDIR); \
+	fi
+
 	mkdir -p $(TARGET_DIR)/etc/thingyjp/ota/keys
-	cp $(BR2_PACKAGE_THINGYJP_OTA_KEYSDIR)/rsa.pub $(TARGET_DIR)/etc/thingyjp/ota/keys/
+	cp $(BR2_PACKAGE_THINGYJP_OTA_KEYSDIR)/rsa.pub \
+		$(TARGET_DIR)/etc/thingyjp/ota/keys/
 endef
 
 define THINGYJP_OTA_STAMPIMAGE
-        mkdir -p $(TARGET_DIR)/etc/thingyjp/ota/
-	$(HOST_DIR)/bin/ota_stamp -t $(TARGET_DIR) -r $(BR2_PACKAGE_THINGYJP_OTA_REPODIR)
+	mkdir -p $(TARGET_DIR)/etc/thingyjp/ota/
+	$(HOST_DIR)/bin/ota_stamp -t $(TARGET_DIR) \
+		-r $(BR2_PACKAGE_THINGYJP_OTA_REPODIR) endif
 endef
 
-THINGYJP_OTA_POST_INSTALL_TARGET_HOOKS += THINGYJP_OTA_INSTALLKEYS
+ifneq ($(BR2_PACKAGE_THINGYJP_OTA_KEYSDIR),"")
+	THINGYJP_OTA_POST_INSTALL_TARGET_HOOKS += THINGYJP_OTA_INSTALLKEYS
+endif
 
 ifeq ($(BR2_PACKAGE_THINGYJP_OTA_STAMP),y)
-	THINGYJP_OTA_TARGET_FINALIZE_HOOKS += THINGYJP_OTA_STAMPIMAGE
+        ifneq ($(BR2_PACKAGE_THINGYJP_OTA_REPODIR),"")
+		THINGYJP_OTA_TARGET_FINALIZE_HOOKS += THINGYJP_OTA_STAMPIMAGE
+	endif
 endif
 
 define THINGYJP_OTA_INSTALL_INIT_SYSV
-        $(INSTALL) -D -m 0755 $(THINGYJP_OTA_PKGDIR)/S91thingyjp-ota $(TARGET_DIR)/etc/init.d/S91thingyjp-ota
+        sed -e s#TMPL_HOST#$(BR_PACKAGE_THINGYJP_OTA_HOST)# \
+		-e s#TMPL_PATH#$(BR_PACKAGE_THINGYJP_OTA_PATH)# \
+		$(THINGYJP_OTA_PKGDIR)/S91thingyjp-ota.template > \
+                $(TARGET_DIR)/etc/init.d/S91thingyjp-ota
+        chmod 755 $(TARGET_DIR)/etc/init.d/S91thingyjp-ota
 endef
 
 $(eval $(meson-package))
--- package/thingymcconfig/thingymcconfig.mk
+++ package/thingymcconfig/thingymcconfig.mk
@@ -17,8 +17,15 @@ THINGYMCCONFIG_CONF_OPTS += -Dwpasupplicantpath=/usr/sbin/wpa_supplicant
 
 define THINGYMCCONFIG_INSTALL_INIT_SYSV
 	sed s/TMPL_INTERFACE/$(BR2_PACKAGE_THINGYMCCONFIG_INTERFACE)/ $(THINGYMCCONFIG_PKGDIR)/S90thingymcconfig.template >\
-		$(TARGET_DIR)/etc/init.d/S90thingmcconfig
-	chmod 755 $(TARGET_DIR)/etc/init.d/S90thingmcconfig
+		$(TARGET_DIR)/etc/init.d/S90thingymcconfig
+	chmod 755 $(TARGET_DIR)/etc/init.d/S90thingymcconfig
 endef
+
+define THINGYMCCONFIG_LINK_RESOLVCONF
+	rm -f $(TARGET_DIR)/etc/resolv.conf
+	ln -s /run/thingymcconfig/resolv.conf $(TARGET_DIR)/etc/resolv.conf
+endef
+
+THINGYMCCONFIG_POST_INSTALL_TARGET_HOOKS += THINGYMCCONFIG_LINK_RESOLVCONF
 
 $(eval $(meson-package))
